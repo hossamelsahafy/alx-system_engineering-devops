425add911b60b339ac2b6433ba3e06dea4a22808
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-23 17:00:13 +0200

    file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# create the file in /tmp
+
 file { '/tmp/school':
-  content => 'I love Puppet'
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content =>'I love Puppet',
 }
