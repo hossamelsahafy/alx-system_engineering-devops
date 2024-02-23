32bcb055584521c0a611758e2dbdaa3458bbc6e6
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-23 18:48:18 +0200

    file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/usr/bin/pup
-# install flask
+#install werkzeug, flask
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
 package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
