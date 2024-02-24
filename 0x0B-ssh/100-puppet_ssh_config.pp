26993818bd671c7dd23a1ac5ddf10f3f326ccd23
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-02-24 19:52:42 +0200

    File

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+#configuration Using pupet
+file {'/home/hossam/.ssh/config':
+    ensure => 'file',
+    mode => '0600',
+    content => "host *\n IdentityFile ~/.ssh/school\n passwordAuthentication no"
+}
