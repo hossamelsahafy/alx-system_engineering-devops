4589d63c15620a2c4a18cf3727ad927a8e70096d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-04-10 01:35:35 +0200

    file

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,8 +5,8 @@ title of all hot articles, and prints a
 sorted count of given keywords (case-insensitive, delimited by spaces
 """
 from collections import Counter
-import requests
 import re
+import requests
 
 
 def count_words(subreddit, word_list, hot_list=[], after=None):
