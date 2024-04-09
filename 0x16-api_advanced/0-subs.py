952c5eee4207e1f681d5371682c4f4110844621b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-04-10 00:13:00 +0200

    File

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'custom'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
