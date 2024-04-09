26bac23926f7f2842c96767b3f751bee5134ac40
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-04-10 00:30:44 +0200

    file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Get The Number Of Subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'custom'}
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/Young_68)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
