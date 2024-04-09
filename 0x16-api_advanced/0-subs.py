80539418e411f3fd6e0e0bd7a118f3af8358b56c
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-04-10 00:07:13 +0200

    File

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns
+the number of subscribers (not active users, total subscribers)
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+    data = response.json()
+    return data['data']['subscribers']
